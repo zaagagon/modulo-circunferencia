f64b7f4ae507310b29dba9f6ac48fa7223d63f83
zaagagon <zaagagon@gmail.com>
2019-10-22 15:01:19 +0000

    cambios en circle.js

--- circle.js
+++ circle.js
@@ -3,10 +3,10 @@ var PI = Math.PI;
 
 //exportamos las funciones area y circumference con exports
 exports.area = function (r){
-return Pi * r * r;
+return PI * r * r;
 };
 
 exports.circumference = function (r){
 return 2 * PI*r;
 };
-
+//completar el ejercicio haciendolo desde un html
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //cargamos el modulo circle
 
-var circle = require('./circle';)
+var circle = require('./circle.js');
 console.log('el area de un circulo con radio 6 es '+ circle.area(6));
 /*var circle = require('./circle.js');
 console.log( 'El área de un círculo con radio 4 es '
